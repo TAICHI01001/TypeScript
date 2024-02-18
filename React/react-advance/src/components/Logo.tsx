cca5dbc52ee8cc9a19e1c46f216182359cc31e14
TaiChi <anothai.0978452316@gmail.com>
2024-02-18 21:24:32 +0700

    create logo component prop

---
+++ React/react-advance/src/components/Logo.tsx
@@ -0,0 +1,15 @@
+interface Image {
+    url: string
+    width?: string
+}
+export default function Logo({ url, width = "10em" }: Image) {
+    const imgStyle = {
+        width: width,
+        height: "auto"
+    }
+    return (
+        <>
+            <img style={imgStyle} src={url} alt="Not image" />
+        </>
+    )
+}
