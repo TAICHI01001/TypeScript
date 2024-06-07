0c112994ee3f19b5dce4a18320433e168e5ad79f
TaiChi <anothai.0978452316@gmail.com>
2024-06-07 23:48:15 +0700

    feat : New way manage route get data mock

--- DSA/src/main.ts
+++ DSA/src/main.ts
@@ -1,16 +1,27 @@
-import { Array } from "./class/Array"
-import { LinkList } from "./class/linkList";
+import { Users } from "./mock/userData"
+import express, { Request, Response } from "express"
 
-let myArray = new Array()
-let myLinkList = new LinkList()
-let myArray1: number[] = [112, 1121];
+const server = express()
+const PORT = 3000
 
-myArray1 = myArray.addData(myArray1, 3)
-console.log(myArray.getData(myArray1));
-myArray1 = myArray.updateData(myArray1, 0, 9)
-console.log(myArray.getData(myArray1));
-myArray1 = myArray.deleteData(myArray1,1)
-console.log(myArray.getData(myArray1));
+async function main() {
+    server.get("/", (req: Request, res: Response) => {
+        res.status(200).json({
+            Users
+        }).end(() => {
+            console.log(Users)
+        }).end(() => {
+            console.table(Users)
+        })
+    })
+    server.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`)
+    })
+}
+main()
+
+
+// console.table(...Users)
 
 
 
@@ -18,15 +29,41 @@ console.log(myArray.getData(myArray1));
 
 
 
-// myArray = addData(myArray, 2);
-// myArray = addData(myArray, 1);
-// console.log(getData(myArray));
 
-// myArray = updateData(myArray, 0, 8);
-// console.log(getData(myArray));
 
-// myArray = deleteData(myArray, 1);
-// console.log(getData(myArray));
+
+
+
+
+
+
+
+
+
+
+// import { Array } from "./class/Array"
+// import { LinkList } from "./class/LinkList";
+
+// let myArray = new Array()
+// let myLinkList = new LinkList()
+// let myArray1: number[] = [112, 1121];
+
+// myArray1 = myArray.addData(myArray1, 3)
+// console.log(myArray.getData(myArray1));
+// myArray1 = myArray.updateData(myArray1, 0, 9)
+// console.log(myArray.getData(myArray1));
+// myArray1 = myArray.deleteData(myArray1,1)
+// console.log(myArray.getData(myArray1));
+
+// // myArray = addData(myArray, 2);
+// // myArray = addData(myArray, 1);
+// // console.log(getData(myArray));
+
+// // myArray = updateData(myArray, 0, 8);
+// // console.log(getData(myArray));
+
+// // myArray = deleteData(myArray, 1);
+// // console.log(getData(myArray));
 
 
 
