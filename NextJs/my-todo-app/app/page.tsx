4aadadbb1de91f57082f30443cc578fbfd3d5e8d
TaiChi <anothai.0978452316@gmail.com>
2024-08-01 01:28:54 +0700

    feat : statement add todo

--- NextJs/my-todo-app/app/page.tsx
+++ NextJs/my-todo-app/app/page.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Todo } from '../app/types/todo';
-import { title } from 'process';
+
 export default function Home() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [newTodo, setNewTodo] = useState<string>('');
@@ -20,23 +20,39 @@ export default function Home() {
   }
 
   const addTodo = async () => {
-    try{
-      const response = await axios.post<Todo>('/api/todos', {title: newTodo});
+    try {
+      const response = await axios.post<Todo>('/api/todos', { title: newTodo });
       setTodos([...todos, response.data]);
       setNewTodo('');
-    }catch(error){
+    } catch (error) {
       console.error(error);
     }
   }
-  
+
   const updateTodo = async (id: number, completed: boolean) => {
-    try{
-      await axios.put(`/api/todos/${id}`, {completed});
-      setTodos(todos.map(todo => todo.id === id ? {...todo, completed} : todo));
-    }catch(error){
+    try {
+      await axios.put(`/api/todos/${id}`, { completed });
+      setTodos(todos.map(todo => todo.id === id ? { ...todo, completed } : todo));
+    } catch (error) {
       console.error(error);
     }
+
+    const deleteTodo = async (id: number) => {
+      try {
+        await axios.delete(`/api/todos/${id}`);
+        setTodos(todos.filter(todo => todo.id !== id));
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
   return (
-    <></>
+    <>
+    <div>
+      <h1>Todo List</h1>
+      <input type="text" value={newTodo} onChange={(e)=> setNewTodo(e.target.value)} />
+      <button onClick={addTodo}>Add Todo</button>
+    </div>
+    </>
   );
 }
