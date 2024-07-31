f23a5c894e915083c43fd2c7c006e2d7215ccaa7
TaiChi <anothai.0978452316@gmail.com>
2024-08-01 01:24:53 +0700

    feat : fn add todo

--- NextJs/my-todo-app/app/page.tsx
+++ NextJs/my-todo-app/app/page.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Todo } from '../app/types/todo';
+import { title } from 'process';
 export default function Home() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [newTodo, setNewTodo] = useState<string>('');
@@ -18,6 +19,15 @@ export default function Home() {
     }
   }
 
+  const addTodo = async () => {
+    try{
+      const response = await axios.post<Todo>('/api/todos', {title: newTodo});
+      setTodos([...todos, response.data]);
+      setNewTodo('');
+    }catch(error){
+      console.error(error);
+    }
+  }
   
   return (
     <></>
