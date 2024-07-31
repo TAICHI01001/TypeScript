3328d7de5ff512cacd60de5072f1458ab3ed4d42
TaiChi <anothai.0978452316@gmail.com>
2024-08-01 01:23:35 +0700

    feat : fetch api use axios

--- NextJs/my-todo-app/app/page.tsx
+++ NextJs/my-todo-app/app/page.tsx
@@ -5,6 +5,20 @@ export default function Home() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [newTodo, setNewTodo] = useState<string>('');
 
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get<Todo[]>('/api/todos');
+      setTodos(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  
   return (
     <></>
   );
