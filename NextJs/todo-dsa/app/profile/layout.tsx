99ce4daf452fe15730a822be627a4a82d2775a9d
TaiChi <anothai.0978452316@gmail.com>
2025-02-04 01:33:15 +0700

    chore : nothing fix code a little bit

---
+++ NextJs/todo-dsa/app/profile/layout.tsx
@@ -0,0 +1,12 @@
+export default function ProfileLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return <>
+        <header>something</header>
+        <section>{children}</section>
+        <footer>Footer in layout profile</footer>
+    </>
+
+}
